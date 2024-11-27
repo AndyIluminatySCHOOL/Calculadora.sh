59c49e38e0fc1b924669dce816fce2f0eb037606
AndyIluminatySCHOOL <andreslos523@gmail.com>
2024-11-26 20:40:25 -0500

    Add files via upload

---
+++ calculadora.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Calculadora básica"
+echo "Ingresa el primer número:"
+read num1
+
+echo "Ingresa el segundo número:"
+read num2
+
+echo "Selecciona una opción:"
+echo "1. Sumar"
+echo "2. Restar"
+read opcion
+
+if [ $opcion -eq 1 ]; then
+  resultado=$((num1 + num2))
+  echo "El resultado de la suma es: $resultado"
+elif [ $opcion -eq 2 ]; then
+  resultado=$((num1 - num2))
+  echo "El resultado de la resta es: $resultado"
+else
+  echo "Opción no válida"
+fi
